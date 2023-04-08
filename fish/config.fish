8a9e561fffd937b89ba0cdc36453b5f98fd0542c
Pathompong Pechkongtong <boynoiz@gmail.com>
2023-04-08 14:29:40 +0700

    move away from wsl2

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,14 @@
+#set PATH
+set PATH $HOME/bin $HOME/.local/bin /usr/local/bin /usr/share $PATH
+
 # Homebrew
 if test -d /home/linuxbrew/.linuxbrew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    set -gx LD_LIBRARY_PATH /usr/lib (brew --prefix)/lib $LD_LIBRARY_PATH
-    set -gx LDFLAGS "-L/home/linuxbrew/.linuxbrew/opt/openssl@3/lib" $LDFLAGS
-    set -gx CPPFLAGS "-I/home/linuxbrew/.linuxbrew/opt/openssl@3/include" $CPPFLAGS
-    set -gx PKG_CONFIG_PATH "/home/linuxbrew/.linuxbrew/opt/openssl@3/lib/pkgconfig" $PKG_CONFIG_PATH
+    #set -gx LD_LIBRARY_PATH /usr/lib (brew --prefix)/lib $LD_LIBRARY_PATH
+    #set -gx LDFLAGS "-L/home/linuxbrew/.linuxbrew/opt/openssl@3/lib" $LDFLAGS
+    #set -gx CPPFLAGS "-I/home/linuxbrew/.linuxbrew/opt/openssl@3/include" $CPPFLAGS
+    #set -gx PKG_CONFIG_PATH "/home/linuxbrew/.linuxbrew/opt/openssl@3/lib/pkgconfig" $PKG_CONFIG_PATH
 end
-
-#set PATH
-set PATH $HOME/bin $HOME/.local/bin /usr/local/bin /usr/share $PATH
 
 #encoding
 set LANG en_US.UTF-8
@@ -21,23 +21,19 @@ set LC_MESSAGES en_US.UTF-8
 set -gx GPG_TTY (tty)
 
 # Windows X-Server
-set -gx GDK_SCALE 0
-set -gx GDK_BACKEND x11
-set -gx LIBGL_ALWAYS_INDIRECT 1
-set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-
-# Fish shell autocompletions
-if test -d (brew --prefix)"/share/fish/completions"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-end
-
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-end
+#set -gx GDK_SCALE 0
+#set -gx GDK_BACKEND x11
+#set -gx LIBGL_ALWAYS_INDIRECT 1
+#set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
 
 # Fish addition functions
 if test -d $HOME/.dotfiles/fish/functions
   set -gx fish_function_path $fish_function_path "$HOME/.dotfiles/fish/functions"
+end
+
+# Fish shell autocompletions
+if test -d $HOME/.dotfiles/fish/completions
+  set -gx fish_complete_path $fish_function_path "$HOME/.dotfiles/fish/completions"
 end
 
 # asdf
@@ -53,16 +49,6 @@ end
 # PyENV
 if type -q python
   set -gx PYTHON_BUILD_ARIA2_OPTS "-x 10 -k 1M"
-end
-
-# SurrealDB
-if type -q "$HOME/.surrealdb/surreal"
-  set -gx PATH $PATH $HOME/.surrealdb
-end
-
-# Tiup
-if test -d "$HOME/.tiup/bin"
-  set -gx PATH $PATH $HOME/.tiup/bin
 end
 
 # Kruw
